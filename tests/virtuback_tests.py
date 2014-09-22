18d66749c6bdd225840be89153bd37cd1f2b16c3
Vilhelm von Ehrenheim <vonehrenheim@gmail.com>
2014-09-22 23:01:00 +0200

    Started to implement routes and tests.

--- tests/virtuback_tests.py
+++ tests/virtuback_tests.py
@@ -1,12 +1,24 @@
 from nose.tools import *
-import virtuback
+from virtuback import app
+from flask import json
 
-def setup():
-    print('SETUP!')
+class TestAPI:
+    def setUp(self):
+        print('SETUP!')
+        app.config['TESTING'] = True
+        self.client = app.test_client()
 
-def teardown():
-    print('TEAR DOWN!')
+    def tearDown(self):
+        print('TEAR DOWN!')
 
-def test_tests():
-    print('I RAN!')
+    def test_404(self):
+        res = self.client.get('/api/v1.0/nottobefound')
+        assert res.status == "404 NOT FOUND"
 
+    def test_user_get(self):
+        res = self.client.get('/api/v1.0/user/1')
+        data = json.loads(res.data)['user']
+        print(data)
+        assert data['id'] == 1
+        assert data['name'] == u'Vilhelm von Ehrenheim'
+        assert data['email'] == u'vonehrenheim@gmail.com'
--- virtuback/api.py
+++ virtuback/api.py
@@ -1,19 +1,21 @@
-from flask import jsonify
+from flask import jsonify, abort, make_response
+from hashlib import sha256
 from virtuback import app
 
 
+# Define a mock users list
 users = [
     {
         'id':        1,
-        'name':      u"Vilhelm von Ehrenheim",
-        'email':     u"vonehrenheim@gmail.com",
-        'password':  u"abc123"
+        'name':      u'Vilhelm von Ehrenheim',
+        'email':     u'vonehrenheim@gmail.com',
+        'password':  sha256(b'abc123').hexdigest()
     },
     {
         'id':        2,
-        'name':      u"Tester Testsson",
-        'email':     u"test@test.com",
-        'password':  u"qwerty"
+        'name':      u'Tester Testsson',
+        'email':     u'test@test.com',
+        'password':  sha256(b'qwerty').hexdigest()
     },
 ]
 
@@ -21,4 +23,44 @@ users = [
 # Set up GET listing of all users !!UNSAFE?
 @app.route('/api/v1.0/users', methods=['GET'])
 def list_users():
-    return jsonify({'users': users})
+    """ Method for listing all users via GET request. """
+    all = [{'id': d['id'], 'name': d['name'], 'email': d['email']} for d in users]
+    return jsonify({'users': all})
+
+
+# GET route for single user
+@app.route('/api/v1.0/user/<int:id>', methods=['GET'])
+def get_user(id):
+    search = filter(lambda d: d['id'] == id, users)
+    try:
+        return jsonify({'user': next(search)})
+    except StopIteration:
+        abort(404)
+
+
+# POST route to insert a new user
+@app.route('/api/v1.0/user/<int:id>', methods=['POST'])
+def insert_user(id):
+    abort(501)
+
+
+# PUT route to update a user
+@app.route('/api/v1.0/user/<int:id>', methods=['PUT'])
+def update_user(id):
+    abort(501)
+
+
+# DELETE route to remove a user
+@app.route('/api/v1.0/user/<int:id>', methods=['DELETE'])
+def remove_user(id):
+    abort(501)
+
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': '404 NOT FOUND'}), 404)
+
+
+@app.errorhandler(501)
+def not_implemented(error):
+        return make_response(jsonify({'error': '501 NOT IMPLEMENTED'}), 501)
