4ef454f342b9862dde14b97a5466b18781a6b44a
Vilhelm von Ehrenheim <vonehrenheim@gmail.com>
2014-09-23 07:32:07 +0200

    Implemented POST user.

--- virtuback/api.py
+++ virtuback/api.py
@@ -1,9 +1,10 @@
-from flask import jsonify, abort, make_response
+from flask import jsonify, abort, request, make_response
 from hashlib import sha256
+import re
 from virtuback import app
 
 
-# Define a mock users list
+# Define a mock users db
 users = [
     {
         'id':        1,
@@ -20,7 +21,9 @@ users = [
 ]
 
 
-# Set up GET listing of all users !!UNSAFE?
+# ------------------------------------------------------------------------------
+#  Set up GET listing of all users !!UNSAFE?
+# ------------------------------------------------------------------------------
 @app.route('/api/v1.0/users', methods=['GET'])
 def list_users():
     """ Method for listing all users via GET request. """
@@ -28,7 +31,9 @@ def list_users():
     return jsonify({'users': all})
 
 
-# GET route for single user
+# ------------------------------------------------------------------------------
+#  GET route for single user
+# ------------------------------------------------------------------------------
 @app.route('/api/v1.0/user/<int:id>', methods=['GET'])
 def get_user(id):
     search = filter(lambda d: d['id'] == id, users)
@@ -38,29 +43,77 @@ def get_user(id):
         abort(404)
 
 
-# POST route to insert a new user
+# ------------------------------------------------------------------------------
+#  POST route to insert a new user
+# ------------------------------------------------------------------------------
 @app.route('/api/v1.0/user/<int:id>', methods=['POST'])
 def insert_user(id):
-    abort(501)
+    if not request.json:
+        abort(400)
+
+    if 'name' not in request.json or request.json['name'] == '':
+        abort_with_reason('Invalid name', 400)
+
+    if ('email' not in request.json or
+        not re.match('^[^@\s]+@[^@\s]+\.[a-zA-Z]+$', request.json['email'])):
+        abort_with_reason('Invalid email', 400)
+
+    if 'password' not in request.json or len(request.json['password']) < 8:
+        abort_with_reason('Password too short! Must be >= 8 characters.', 400)
+
+    new = {
+        'id': len(users) + 1,
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': sha256(request.json['password'].encode('UTF-8')).hexdigest()
+    }
+    users.append(new)
+
+    return jsonify({'user': new}), 201
 
 
-# PUT route to update a user
+# ------------------------------------------------------------------------------
+#  PUT route to update a user
+# ------------------------------------------------------------------------------
 @app.route('/api/v1.0/user/<int:id>', methods=['PUT'])
 def update_user(id):
+    search = filter(lambda d: d['id'] == id, users)
+    try:
+        user = search.next()
+    except StopIteration:
+        abort(404)
     abort(501)
 
 
-# DELETE route to remove a user
+# -------------------------------------------------------------------------------
+#  DELETE route to remove a user
+# -------------------------------------------------------------------------------
 @app.route('/api/v1.0/user/<int:id>', methods=['DELETE'])
 def remove_user(id):
     abort(501)
 
 
+# ------------------------------------------------------------------------------
+#  Send abort response with a reason
+# ------------------------------------------------------------------------------
+def abort_with_reason(reason, code):
+    response = make_response(jsonify({'error': reason}))
+    response.status_code = code
+    response.headers = {"X-Status-Reason": reason}
+    abort(response)
+
+
+# ------------------------------------------------------------------------------
+#  404 not found response
+# ------------------------------------------------------------------------------
 @app.errorhandler(404)
 def not_found(error):
         return make_response(jsonify({'error': '404 NOT FOUND'}), 404)
 
 
+# ------------------------------------------------------------------------------
+#  501 not implemented response
+# ------------------------------------------------------------------------------
 @app.errorhandler(501)
 def not_implemented(error):
         return make_response(jsonify({'error': '501 NOT IMPLEMENTED'}), 501)
